be22ba91279cefa01ee9dd05668a77fe3ca3a823
Guo <1780465535@qq.com>
2023-05-10 23:34:25 +0800

    PubActivity & KnifeActicity

---
+++ frontend/app/src/main/java/com/android/campusmoments/Activity/KnifeActivity.java
@@ -0,0 +1,14 @@
+package com.android.campusmoments.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class KnifeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_knife);
+    }
+}
