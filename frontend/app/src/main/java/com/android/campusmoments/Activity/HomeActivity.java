b6b7fd15ee8bf0ec20a169c808364e11ef3aed65
Guo <1780465535@qq.com>
2023-05-25 23:43:20 +0800

    顶部导航栏
    TabLayout 配合 ViewPager2

--- frontend/app/src/main/java/com/android/campusmoments/Activity/HomeActivity.java
+++ frontend/app/src/main/java/com/android/campusmoments/Activity/HomeActivity.java
@@ -79,18 +79,5 @@ public class HomeActivity extends AppCompatActivity {
             }
             return false;
         });
-        // TODO: setOnItemReselectedListener
-        bottomNavigationView.setOnItemReselectedListener(item -> {
-            switch (item.getItemId()) {
-                case R.id.home:
-                    homeFragment.refresh();
-                    break;
-                case R.id.message:
-                    break;
-                case R.id.my:
-                    myFragment.refresh();
-                    break;
-            }
-        });
     }
 }
